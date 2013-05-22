0c1c46dec909136480b136a3d3b5d8e1092e0af6
Kaloyan Yordanov <kaloyan.yordanov@dbrdev.com>
2013-05-22 17:15:36 +0300

    Products Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileApp::Application.config.secret_token = '90f9deb7efa0cd10756d8c1128ddb59778e48954f9ecef7d5c7bb386c0edd0c98c0e3c00580e1b5781cfbb167ecbc8c8a5a917e98c3ca4e95f5746ce5686bb97'
